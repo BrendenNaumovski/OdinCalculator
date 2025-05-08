b84794680fd02f0543f2c24c9f68644e3c0781cb
Brenden Naumovski <bnaumovski@proton.me>
2025-05-08 10:28:55 -0400

    Calculator functional.

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
 function clear() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     input = [];
     opIndex = -1;
     document.getElementById("display").value = '';
+    console.log("New Array: " + input);
 }
 
 function negate() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     if (input.length === 0) return;
     let lastNum = input.slice(opIndex + 1);
+    oldLength = lastNum.length;
+    lastNum = lastNum.join('');
     if (lastNum[0] === '-') {
-        lastNum.shift();
+        lastNum = lastNum.slice(1);
     } else {
-        lastNum.unshift('-');
+        lastNum = '-' + lastNum;
     }
-    input.splice(opIndex + 1, lastNum.length, ...lastNum);
+    input.splice(opIndex + 1, oldLength, ...[lastNum]);
     document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function percent() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     if (input.length === 0) return;
-    let lastNum = input.slice(opIndex + 1);
-    if (lastNum[0] === '-') {
-        lastNum.shift();
-    }
-    let percentValue = parseFloat(lastNum.join('')) / 100;
-    input.splice(opIndex + 1, lastNum.length, percentValue);
+    let lastNum = input.slice(opIndex + 1).join('');
+    let percentValue = parseFloat(lastNum) / 100;
+    input.splice(opIndex + 1, 1, percentValue);
     document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function backspace() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     if (input.length === 0) return;
     if (input.length - 1 === opIndex) {
         opIndex = -1;
+        input.pop();
+    } else {
+        let lastNum = input.slice(opIndex + 1).join('');
+        if (lastNum.length === 2 && lastNum[0] === '-') {
+            lastNum = '';
+        } else {
+            lastNum = lastNum.slice(0, -1);
+        }
+        input.splice(opIndex + 1, 1, ...[lastNum]);
     }
-    input.pop();
-    document.getElementById("display").value = input.join('');  
+    document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function operate(a, b, operator) {
@@ -57,48 +76,67 @@ function operate(a, b, operator) {
 }
 
 function inputNum() {
-    input.push(this.innerText);
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
+    let lastNum = input.slice(opIndex + 1);
+    lastNum.push(this.innerText);
+    lastNum = lastNum.join('');
+    input.splice(opIndex + 1, 1, ...[lastNum]);
     document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function inputOperator() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     if (input.length === 0) return;
     if (isNaN(input[input.length - 1])) {
         input[input.length - 1] = this.innerText;
     } else {
+        if (opIndex !== -1) {
+            calculate();
+        }
         input.push(this.innerText);
     }
     opIndex = input.length - 1;
     document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function inputDecimal() {
-    if (input.length === 0 || isNaN(input[input.length - 1])) {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
+    if (input.length === 0 || (input[input.length - 1] !== '.' && isNaN(input[input.length - 1]))) {
         input.push('0.');
     } else {
-        let lastNum = input.slice(opIndex + 1);
+        let lastNum = input.slice(opIndex + 1).join('');
         let decPoint = lastNum.indexOf('.');
         if (decPoint === -1) {
-            input.push('.');
-        } else {
-            input.splice(decPoint, 1)
-            input.push('.');
+            lastNum = lastNum + '.';
+            input.splice(opIndex + 1, 1, ...[lastNum]);
         }
     }
     document.getElementById("display").value = input.join('');
+    console.log("New Array: " + input);
 }
 
 function calculate() {
+    console.log("Button Pressed: " + this.innerText)
+    console.log("Old Array: " + input);
     let num1 = input.slice(0, opIndex).join('');
     let operator = input[opIndex];
     let num2 = input.slice(opIndex + 1).join('');
+    if (num1 === '' || num2 === '') {
+        return;
+    }
     let result = operate(num1, num2, operator);
-    input = [result];
+    input = [String(result)];
     opIndex = -1;
     if (result === "Error") {
         input = [];
     }
     document.getElementById("display").value = result;
+    console.log("New Array: " + input);
 }
 
 let input = [];
